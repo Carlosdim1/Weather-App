fd1dd5ca6905fad6976f4c0858df2e46020e1565
Carlos Jiménez Consuelo <carlosdim_@hotmail.com>
2024-11-16 17:36:25 +0100

    Adding documentation to main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""Main module for the Weather App.
+This module contains the main function that runs the WeatherApp."""
 from interface import WeatherApp
 
 
 def main():
+    """Main function that Runs the WeatherApp """
     app = WeatherApp()
     app.run()
 
